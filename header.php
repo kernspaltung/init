dab64bc67acdfff6bed35e49a86271f7d19e4ef9
josecaos <jazzvoon@gmail.com>
2017-03-06 00:01:53 -0600

    primeros archivos

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plantilla X</title>
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    <main id="main" class="row">
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once 'header.php';
+
+
+
+
+echo 'sii';
+
+
+
+
+
+include_once 'footer.php';
